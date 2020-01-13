ee7d0c2a5eb140ea89362faced504c45370d98b4
whdgusdl48 <52066712+whdgusdl48@users.noreply.github.com>
2020-01-13 21:37:52 +0900

    주니어 창작 편의시설 구현

--- 종현.expressnode/express.js
+++ 종현.expressnode/express.js
@@ -3,7 +3,6 @@ var bodyParser = require('body-parser');
 var fs = require('fs');
 var mysql = require('mysql');
 var app = express();
-
 const connection = mysql.createConnection({
   host : 'localhost',
   user : 'root',
@@ -13,20 +12,18 @@ const connection = mysql.createConnection({
 connection.connect();
 var sql = 'INSERT INTO admin (name,number,selector,password) VALUES(?,?,?,?)';
 
-
 app.use(bodyParser.urlencoded({extended : false}));
 app.use(bodyParser.json());
-app.set('view engine','jade');
-app.set('abc','./views');
+app.set('view engine','ejs');
+app.set('views','./views');
+app.use(express.static(__dirname + '/'));
 
 app.get('/naver',function(req,res){
   res.writeHead(202,{"Content-Type":"text/html; charset=utf-8" });
   fs.createReadStream("./naver.html").pipe(res);
 })
 app.use(express.static('../public'));
-app.get('/views',function(req,res){
-  res.render('form');
-})
+
 app.get('/',function(req ,res){
   res.writeHead(202,{"Content-Type":"text/html; charset=utf-8" });
   fs.createReadStream("./초기화면.html").pipe(res);
@@ -37,9 +34,15 @@ app.post('/form_receive',function(req,res){
   var pass = req.body.passwd;
   var selector = req.body.selector;
   var param = [name,number,selector,pass];
+  var sql2 = `CREATE TABLE ${name} (월 VARCHAR(255) NOT NULL, 일정기간 VARCHAR(255) NOT NULL, 일정 VARCHAR(255) NOT NULL)`;
+  var param2 = name;
   connection.query(sql,param,function(err,row,field){
     if(err) throw err;
     console.log(row);
+  })
+  connection.query(sql2,function(err,row,field){
+    if(err) throw err;
+    console.log(1);
   })
   res.send(`회원가입되었습니다!!
             <a href ="/">돌아가기 </a>`);
@@ -47,27 +50,29 @@ app.post('/form_receive',function(req,res){
 app.post('/successlogin',function(req,res){
   var number = req.body.number1;
   var password = req.body.passwd;
+  var selector1;
   var sql2 = 'SELECT * FROM admin';
-
+  var name;
   connection.query(sql2,'utf8',function(err,row,field){
     if(err) throw err;
     else{
       var check;
       for(var i = 0; i<row.length;i++){
         if(number == row[i].number && password == row[i].password){
-          console.log(number == row[i].number);
-          console.log(password == row[i].password);
+          name = row[i].name;
+          selector1 = row[i].selector;
           check = 1;
           break;
-        }
-        else{
-          console.log(number == row[i].number);
-          console.log(password == row[i].password);
-          check = 2;
-        }
+        }else{check = 2;}
       }
       if(check === 1){
-            res.send(`<script>alert('로그인되었습니다!')</script>`);
+        console.log(selector1);
+        console.log(number);
+        res.render('a',{
+          name : name,
+          number : parseInt(number),
+          selector : selector1,
+        });
         }
         else{
             res.send(`<script>alert('로그인에 실패하였습니다.');
@@ -79,7 +84,7 @@ app.post('/successlogin',function(req,res){
 })
 app.locals.pretty = true;
 
-app.get('/html',function(req,res){
+app.post('/html',function(req,res){
   res.writeHead(202,{"Content-Type":"text/html; charset=utf-8" });
   fs.createReadStream("./회원가입.html").pipe(res);
 })
